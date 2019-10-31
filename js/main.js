0f63ac61a9e06853b766b50b20eb1706f611359c
OnMaine <Onmaine@gmail.com>
2019-10-31 14:03:24 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-   /* Портфолио */
+   /* portfolio */
    $('.button[filter]').click(function () {
       if ($(this).attr('val') == 'off') {
          $('.button[filter]').attr('val', 'off').removeClass('focused');
@@ -20,3 +20,17 @@ var btn = document.querySelectorAll('button');
 for (var i = 0; i < btn.length; i++) {
    btn[i].style.outline = 'none';
 }
+
+/* drop-menu */
+$('.menu-icon').click(function () {
+   $('nav').slideToggle;
+   $('ul.menu').css({
+      'display': 'flex',
+      'flex-direction': 'column'
+   });
+   if ($('.menu-icon').html() == '<i class="fas fa-bars"></i>') {
+      $(this).html('<i class="fas fa-times"></i>');
+   } else {
+      $(this).html('<i class="fas fa-bars"></i>');
+   }
+});
